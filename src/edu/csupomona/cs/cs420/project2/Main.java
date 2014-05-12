1b5fd458d88b7469a2b55059a1f7b511d74783bb
Collin Smith <collinsmith@csupomona.edu>
2014-05-12 14:35:14 -0700

    Update Main.java
    Added comments!

--- src/edu/csupomona/cs/cs420/project2/Main.java
+++ src/edu/csupomona/cs/cs420/project2/Main.java
@@ -19,12 +19,25 @@ import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 /**
+ * Main class of the n-Queen solver program. This program will ask
+ * for various information about the type of n-Queen problem that
+ * needs to be solved, and then solve it for you.
  * 
  * @author Collin Smith (collinsmith@csupomona.edu)
  */
 public class Main {
+	/**
+	 * Random number generator used for randomizing the initial
+	 * queen locations, as well as various other tasks throughout
+	 * the program, such as choosing a random crossover point.
+	 */
 	private static final Random RAND = new Random();
 
+	/**
+	 * Main method executed at program start.
+	 * 
+	 * @param args arguments passed to the program (none expected)
+	 */
 	public static void main(String[] args) {
 		final Scanner SCAN = new Scanner(System.in);
 		System.out.format("What size board do you want to use? ");
@@ -111,6 +124,14 @@ public class Main {
 		System.out.format("Solution Set (Iteration #): %s%n", Arrays.toString(solutions.toArray(new Integer[0])));
 	}
 
+	/**
+	 * Generates a random n-queen board by placing a queen in the diagonal
+	 * and then swapping each position randomly.
+	 * 
+	 * @param N size of the board
+	 * 
+	 * @return the randomly generated board
+	 */
 	private static int[] generateRandomBoard(final int N) {
 		int[] BOARD = new int[N];
 		for (int i = 0; i < BOARD.length; i++) {
@@ -126,6 +147,13 @@ public class Main {
 		return BOARD;
 	}
 
+	/**
+	 * Swaps two indeces in the array.
+	 * 
+	 * @param arr the array to swap
+	 * @param i the first index to swap
+	 * @param j the index to swap it with
+	 */
 	private static void swap(int[] arr, int i, int j) {
 		if (i == j) {
 			return;
@@ -136,6 +164,15 @@ public class Main {
 		arr[i] ^= arr[j];
 	}
 
+	/**
+	 * Runs a steepes hill-climbing algorithm with the given initial
+	 * state and returning the resulting states.
+	 * 
+	 * @param initialState initial board
+	 * @param printPaths whether or not to print the paths
+	 * 
+	 * @return a result object containing the results
+	 */
 	private static Result runHillClimbing(int[] initialState, boolean printPaths) {
 		Node neighbor;
 		List<Node> betterSuccessors;
@@ -167,6 +204,15 @@ public class Main {
 		}
 	}
 
+	/**
+	 * Runs a genetic algorithm on a list of given initial states and
+	 * performs generations of genetic crossovers and mutations until
+	 * a solution is found.
+	 * 
+	 * @param initialStates list of initial states
+	 * 
+	 * @return a result object containing statistical information
+	 */
 	private static Result runGenetic(List<Node> initialStates) {
 		Node[] crossover;
 		Node highestFitness;
@@ -201,11 +247,24 @@ public class Main {
 		}
 	}
 	
+	/**
+	 * This class contains methods associated with the genetic alogorithm.
+	 */
 	private static class Genetic {
+		/**
+		 * Comparator which can determine which nodes are better than others.
+		 */
 		static final Comparator<Node> FITNESS_FUNCTION = (Node n1, Node n2) -> {
 			return n1.COST - n2.COST;
 		};
 		
+		/**
+		 * Selects the best {@code n/2 + 1} elements
+		 * 
+		 * @param currentGeneration the generation to perform the selection from
+		 * 
+		 * @return a list containing the best elements according o {@link #FITNESS_FUNCTION}
+		 */
 		static List<Node> select(List<Node> currentGeneration) {
 			assert (currentGeneration.size()&1) == 0 : "Generation sizes should be even!";
 			Collections.sort(currentGeneration, FITNESS_FUNCTION);
@@ -217,6 +276,14 @@ public class Main {
 			return nextGeneration;
 		}
 		
+		/**
+		 * Performs a crossover between two random nodes using a random point
+		 * 
+		 * @param n1 first node to crossover
+		 * @param n2 the other node to crossover n1 with
+		 * 
+		 * @return an array containing the two crossed over nodes
+		 */
 		static Node[] crossover(Node n1, Node n2) {
 			assert n1.BOARD.length == n2.BOARD.length;
 			final int N = n1.BOARD.length;
@@ -235,6 +302,13 @@ public class Main {
 			return new Node[] { n12, n21 };
 		}
 		
+		/**
+		 * Performs a mutation operation on the given node with {@code P=1}
+		 * 
+		 * @param n the node to mutate
+		 * 
+		 * @return the result of the mutation
+		 */
 		static Node mutate(Node n) {
 			int[] mutated = Arrays.copyOf(n.BOARD, n.BOARD.length);
 			final int POS = RAND.nextInt(mutated.length);
@@ -243,12 +317,39 @@ public class Main {
 		}
 	}
 
+	/**
+	 * This class contains various informational statisics resulting from
+	 * an algoithm.
+	 */
 	private static final class Result {
+		/**
+		 * Final state of the algorithm
+		 */
 		final Node TERMINAL_STATE;
+		
+		/**
+		 * Number of moves/steps the algorithm took
+		 */
 		final int NUM_MOVES;
+		
+		/**
+		 * Initial start time
+		 */
 		final long START_TIME;
+		
+		/**
+		 * End time, inferred when object created
+		 */
 		final long END_TIME;
 
+		/**
+		 * Constructs a Result object using the given information
+		 * that the algorithm should have.
+		 * 
+		 * @param terminalState final state of the algorithm
+		 * @param numMoves number of moves the algorithm as taken
+		 * @param startTime time the algorithm started
+		 */
 		Result(Node terminalState, int numMoves, long startTime) {
 			this.TERMINAL_STATE = terminalState;
 			this.NUM_MOVES = numMoves;
@@ -257,14 +358,35 @@ public class Main {
 		}
 	}
 
+	/**
+	 * This class represents a node used within the n-queen problem
+	 */
 	private static final class Node {
+		/**
+		 * Board contianing the location of the queens
+		 */
 		final int BOARD[];
+		
+		/**
+		 * "cost" of this board
+		 */
 		final int COST;
 
+		/**
+		 * Constructs a node using the specified board and inferring the cost.
+		 * 
+		 * @param board array containing the location of the queens
+		 */
 		Node(int[] board) {
 			this(board, countAttacking(board));
 		}
 
+		/**
+		 * Constructs a node using the specified board with a specified cost.
+		 * 
+		 * @param board array containing the location of the queens
+		 * @param cost the cost of this node
+		 */
 		Node(int[] board, int cost) {
 			this.BOARD = board;
 			this.COST = cost;
@@ -288,6 +410,12 @@ public class Main {
 			return new Node(NEW_BOARD);
 		}
 
+		/**
+		 * Returns a list containing successors which have a lower cost
+		 * than this node.
+		 * 
+		 * @return list containing successors which are better
+		 */
 		List<Node> generateBetterSuccessors() {
 			if (COST == 1) {
 				return Collections.EMPTY_LIST;
@@ -329,6 +457,15 @@ public class Main {
 			return bestSuccessors;
 		}
 
+		/**
+		 * Returns the number of queens attacking eachother on a
+		 * given board up to a certain index.
+		 * 
+		 * @param BOARD the board to check
+		 * @param i the maximum index to check
+		 * 
+		 * @return the number of attacking queens
+		 */
 		static int countAttackingIth(final int[] BOARD, final int i) {
 			int count = 0;
 			final int CACHE = BOARD[i];
@@ -345,10 +482,25 @@ public class Main {
 			return count;
 		}
 
+		/**
+		 * Returns the number of attacking queens up to a certain
+		 * index.
+		 * 
+		 * @param i the maximum index to check
+		 * 
+		 * @return the number of attacking queens
+		 */
 		int countAttackingIth(final int i) {
 			return countAttackingIth(BOARD, i);
 		}
 
+		/**
+		 * Returns the number of attacking queens.
+		 * 
+		 * @param BOARD the board o check
+		 * 
+		 * @return the number of attacking queens
+		 */
 		static int countAttacking(final int[] BOARD) {
 			int count = 0;
 			for (int i = 0; i < BOARD.length; i++) {
@@ -358,10 +510,20 @@ public class Main {
 			return count;
 		}
 
+		/**
+		 * Returns the number of attacking queens.
+		 * 
+		 * @retun the number of attacking queens wihin this node.
+		 */
 		int countAttacking() {
 			return countAttacking(BOARD);
 		}
 
+		/**
+		 * Returns a string representing this node.
+		 * 
+		 * @return a string representing this node's board.
+		 */
 		@Override
 		public String toString() {
 			StringBuilder sb = new StringBuilder();
